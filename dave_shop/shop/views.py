71c46057872893f1e6330e63c18e4d89853d8ee0
DaveenDev <daveendev07@gmail.com>
2023-03-09 09:53:09 +0800

    added pagination on homepage product list

--- dave_shop/shop/views.py
+++ dave_shop/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -11,6 +12,11 @@ def index(request):
     if search_item != '' and search_item is not None:
         products = products.filter(title__icontains=search_item)
 
+    paginator = Paginator(products, 4)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+
     return render(request, 'shop/index.html', {
-        'products': products
+        'products': products,
+        'search_keyword': search_item
     })
